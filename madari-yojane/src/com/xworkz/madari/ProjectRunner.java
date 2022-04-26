1358bab219963fa966b34df6af1bcd343cd1199d
unknown <xworkzodc@gmail.com>
2022-04-26 13:12:54 +0530

    test push

--- madari-yojane/src/com/xworkz/madari/ProjectRunner.java
+++ madari-yojane/src/com/xworkz/madari/ProjectRunner.java
@@ -3,8 +3,7 @@ package com.xworkz.madari;
 public class ProjectRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("running main");
 	}
 
 }
